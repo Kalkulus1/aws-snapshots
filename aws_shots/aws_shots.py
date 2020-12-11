032382d7f8333416e61122bd95d9b43e78450518
Mumuni Mohammed <mumunim@gmail.com>
2020-12-11 23:33:18 +0000

    Show all snapshots in list if --all is given.

--- aws_shots/aws_shots.py
+++ aws_shots/aws_shots.py
@@ -26,7 +26,8 @@ def snapshots():
 
 @snapshots.command('list')
 @click.option('--project', '-p', default=None, help="Only snapshots for project (tag Project:<name>)")
-def list_snapshots(project):
+@click.option('--all', 'list_all', default=False, is_flag=True, help="List all snapshots for each volume, not just the most recent")
+def list_snapshots(project, list_all):
     "List EC2 Volumes Snapshots"
     
     instances = filter_instances(project)
@@ -42,6 +43,9 @@ def list_snapshots(project):
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+                
+                # Getting just the most current snapshot for a particular instance.
+                if s.state == 'completed' and not list_all: break
     return
 
 @cli.group('volumes')
